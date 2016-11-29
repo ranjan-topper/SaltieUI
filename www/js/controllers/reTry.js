d9dbbd2e44308116465abf83ce13f2db35878c4e
eswaran <Eswaran.Pragasam@zapcg.com>
2016-11-29 18:40:47 +0530

    29-11 TaskBuild

--- www/js/controllers/reTry.js
+++
@@ -1,8 +0,0 @@
-app.controller('RetryController', function($rootScope) {
-  $rootScope.retry = function() {
-    $rootScope.retry.hide();
-  };
-  $rootScope.cancel = function() {
-    $rootScope.retry.remove();
-  };
-});
